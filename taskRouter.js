e71555d24a6b46cd5f578ed223fef4345bf33acd
NegroPrimo123 <pereverzevm44@gmail.com>
2025-06-30 10:41:04 +0300

    Delete taskRouter.js

--- taskRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-
-const taskController = require('../Controllers/taskController')
-
-router.get('/getTask', taskController.getAllTask)
-router.post('/createTask', taskController.createTask)
-
-module.exports = router
